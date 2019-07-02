14bc7c10f42167a04823721c3c6ce6387a239545
Faker Skandran <fskandrani@sierrawireless.com>
2019-07-02 10:17:35 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 import './App.css';
 
 const tasks = [
-        {name: "Learn React", done: false},
-        {name: "Learn CSS", done: true},
-        {name: "Web development", done: true}
-    ];
+  { name: 'Learn React', done: false },
+  { name: 'Learn CSS', done: true },
+  { name: 'Web development', done: true }
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { tasks: tasks, value: '' };
+
+    this.clickHandler = this.clickHandler.bind(this);
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+  }
+
+  clickHandler() {
+    console.log('Yay');
+
+    this.setState(prevState => {
+      const newState = { ...prevState };
+      return newState.tasks.push({ name: this.state.value, done: 'false' });
+    });
+  }
+
+  onChangeHandler(event) {
+    const value = event.target.value;
+    this.setState(prevState => {
+      const newState = { ...prevState };
+      newState.value = value;
+      return newState;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {tasks.map(task => {
+          return <TodoItem key={task.name} done={task.done} name={task.name} />;
+        })}
+        <form onSubmit={this.clickHandler}>
+          <input type="text" id="name" name="name" onChange={this.onChangeHandler} value={this.state.value} />
+          <input type="submit" value="Add" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
